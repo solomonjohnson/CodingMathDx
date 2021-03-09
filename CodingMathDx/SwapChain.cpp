968cf2d41a40eb7697e3b3e85476334db661c7ff
solomon <solomonjohnson@live.in>
2021-03-09 17:10:04 +0530

    Fixed color not coming rit

--- CodingMathDx/SwapChain.cpp
+++ CodingMathDx/SwapChain.cpp
@@ -12,7 +12,7 @@ bool SwapChain::init(HWND hwnd, UINT width, UINT height)
 	desc.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
 	desc.BufferDesc.RefreshRate.Numerator = 60;
 	desc.BufferDesc.RefreshRate.Denominator = 1;
-	desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;;
+	desc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
 	desc.OutputWindow = hwnd;
 	desc.SampleDesc.Count = 1;
 	desc.SampleDesc.Quality = 0;
